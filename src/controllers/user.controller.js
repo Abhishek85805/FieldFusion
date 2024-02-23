21bca7fb0bc4782678a59e5f8293057d566e1c3c
Abhishek <abhishekchauhan85805@gmail.com>
2024-02-23 14:10:52 +0530

    getCurrentUser route is added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -194,10 +194,23 @@ const changeCurrentPassword = asyncHandler(async(req, res) => {
     );
 });
 
+const getCurrentUser = asyncHandler(async(req, res) => {
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            req.user,
+            "User fetched successfully"
+        )
+    )
+});
+
 export {
     register,
     login, 
     logout,
     refreshAccessToken,
-    changeCurrentPassword
+    changeCurrentPassword,
+    getCurrentUser
 };
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,7 +5,8 @@ import {
     login,
     logout,
     refreshAccessToken,
-    changeCurrentPassword
+    changeCurrentPassword,
+    getCurrentUser
 } from "../controllers/user.controller.js";
 import verifyJWT from "../middlewares/auth.middleware.js";
 
@@ -24,5 +25,6 @@ router.route("/login").post(login);
 router.route("/logout").post(verifyJWT, logout);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
 
 export default router;
